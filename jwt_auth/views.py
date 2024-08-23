2ff2bf5bd43e6ff34ae1e1ebaf0ba8e5c88b38b3
User Name <annakirath@gmail.com>
2024-08-23 15:10:35 +0100

    finished the sign up view, now users can sign up

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from .serializers.common import UserSerializer
+from utils.decorators import handle_exceptions
+User = get_user_model()
 
 # Create your views here.
+#Sign up view for creating a user
+class SignUpView(APIView):
+    @handle_exceptions
+    def post(self, request):
+        user_to_create = UserSerializer(data=request.data)
+        if user_to_create.is_valid():
+            user_to_create.save()
+            return Response({ 'message': 'Sign Up Successful'}, 201)
+        return Response(user_to_create.errors, 400)
--- project/urls.py
+++ project/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('flowerposts/', include('flowerposts.urls')),
     path('comments/', include('comments.urls')),
-    path('categories/', include('categories.urls'))
+    path('categories/', include('categories.urls')),
+    path('auth/', include('jwt_auth.urls'))
 ]
